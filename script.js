a98ff6907d1d057687efcfd799cac20b3faaa353
GavWes118Money <Westry.G182924@stu.sanjac.edu>
2026-05-07 08:13:27 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const title = document.getElementById("title");
+const button = document.getElementById("changeBtn");
 
+button.addEventListener("click", () => {
+  title.style.color = "#0c96f2";
+});
